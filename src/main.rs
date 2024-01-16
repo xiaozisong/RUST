ba958e70cda5678be0d0d25879b5178491fd0d1b
xiaozisong <1170957523@qq.com>
2024-01-16 23:21:16 +0800

    了解struct

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,24 @@
-// struct
+// 使用接口
+#[derive(Debug)]
+// 利用struct
+struct Rectangle {
+  width: u32,
+  height: u32,
+}
 fn main () {
-  struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-  }
-  // 如果想更新struct里面的属性值，可以使用mut关键字进行标识，但是rust不允许只修改某一个字段
-  // 用mut标识后所有成员字段都可以被修改
-  let mut user = User {
-    username: String::from("shawn"),
-    email: String::from("123@gmail.com"),
-    active: true,
-    sign_in_count: 556,
-  };
-  // 和其他语言中访问属性的形式是一样的，都是通过  “.” 
-  user.email = String::from("345@gmail.com");
-
-  // struct的更新方法  如果想使用前一个struct的值，不需要这么繁琐，写法和作用与js差不多 通过..的方式进行赋值
-  let user2 = User {
-    username: String::from("zis"),
-    email: String::from("567@gmail.com"),
-    active: user.active,
-    sign_in_count: user.sign_in_count,
+  // 使用一个函数，输出长方形的面积
+  let Rect = Rectangle {
+    width: 30,
+    height: 50,
   };
 
-  let user3 = User {
-    username: String::from("lisi"),
-    email: String::from("798@gmail.com"),
-    ..user2
-  };
+  println!("{}", area(&Rect));
+  // 如何打印struct 直接打印会报错，需要借用一个接口
+  // println!("{}", Rectangle);
+  // 调用接口后，再打印, 使用#可以使打印的值更加清晰
+  println!("{:#?}", Rect);
+}
 
-  // tuple struct 元祖struct 和元祖的使用方法一样，可以解构可以通过索引访问
-  struct Color (i32, i32, i32);
-  struct Point (i32, i32, i32);
-  let color = Color(1,2,3);
-  let origin = Point(0, 0, 0);
-  let color1 = color.0;
-  let (a, b, c) = (1,2,3);
-
+fn area (rect: &Rectangle) -> u32 {
+  rect.width * rect.height
 }
